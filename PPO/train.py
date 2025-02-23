5ddf5e2d0f962f699b6912086eb6ef6c697d9836
Ayush B <102773944+AyushB08@users.noreply.github.com>
2025-02-22 17:07:13 -0800

    Update train.py

--- PPO/train.py
+++ PPO/train.py
@@ -25,16 +25,22 @@ env = CarEnv()
 env.reset(seed=None)
 print('Env has been reset as part of launch')
 model = PPO('MlpPolicy', env, verbose=1,
-           learning_rate=0.0003, 
-           n_steps=2048, 
-           batch_size=64,
+           learning_rate=0.00005, 
+           n_steps=2048,
+           batch_size=256,  
+           gamma=0.995,  
+           gae_lambda=0.98, 
+           clip_range=0.1,  
+           ent_coef=0.005, 
+           vf_coef=1.0, 
+           max_grad_norm=0.3, 
            tensorboard_log=logdir)
 
-TIMESTEPS = 750_000 
+TIMESTEPS = 1_500_000  
 iters = 0
-while iters<4: 
+while iters < 6: 
     iters += 1
     print('Iteration ', iters,' is to commence...')
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO" )
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name=f"PPO")
     print('Iteration ', iters,' has been trained')
     model.save(f"{models_dir}/{TIMESTEPS*iters}")
